dfe5e276eb803d02b8f121a9359b60c060ce55d6
Ruslana-P <pavliuk_r_v@protonmail.com>
2022-12-22 15:11:08 +0100

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const burger = document.querySelector('.nav-burger');
-const nav = document.querySelector('.header__nav');
-const closeButton = document.querySelector('.nav-close-button');
-burger.addEventListener('click', function() {
-    nav.style.display = 'block';
-})
-closeButton.addEventListener('click', function () {
-    nav.style.display = 'none';
-})
+const burger = document.querySelector(".nav-burger");
+const nav = document.querySelector(".header__nav");
+const closeButton = document.querySelector(".nav-close-button");
+burger.addEventListener("click", function () {
+  nav.style.display = "block";
+});
+closeButton.addEventListener("click", function () {
+  nav.style.display = "none";
+});
